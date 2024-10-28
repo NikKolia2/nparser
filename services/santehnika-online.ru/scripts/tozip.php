87a245c2618f42f1cd7a64f04ed03b868bb6cdb3
Nikola <nikola.vls@uandex.ru>
2024-10-28 13:19:31 +0400

    fix

--- services/santehnika-online.ru/scripts/tozip.php
+++ services/santehnika-online.ru/scripts/tozip.php
@@ -13,40 +13,47 @@ if($total%$limit){
     $pages += 1;
 }
 
-echo $pages;
-// $processes = $process->query()->select()->from($process::getTableName())->where("status_id", 4)->paginationLimit($page, $limit)->fetchArrays();
-// $files = [];
-// foreach($processes as $p){
-//     $url = $p["url"];
-//     $filename = hash("sha256", $url);
-//     $filepath = dirname(__DIR__, 3)."/storage/html/$filename.html";
-//     $files[] = $filepath;
-// }
+for($i = 1; $i <= $pages; $i++){
+    $processes = $process->query()->select()->from($process::getTableName())->where("status_id", 4)->paginationLimit($i, $limit)->fetchArrays();
+    $files = [];
+    foreach($processes as $p){
+        $url = $p["url"];
+        $filename = hash("sha256", $url);
+        $filepath = dirname(__DIR__, 3)."/storage/html/$filename.html";
+        $files[] = $filepath;
+    }
 
-// $zip = new ZipArchive();
+    $zip = new ZipArchive();
 
-// # create a temp file & open it
-// $create = $zip->open(__DIR__ . '/archive'.date("Y-m-d_H-i-s").'.zip', ZipArchive::CREATE);
+    # create a temp file & open it
+    $create = $zip->open(__DIR__ . '/archive'.date("Y-m-d_H-i-s").'.zip', ZipArchive::CREATE);
 
-// if ( $create === TRUE) {
-//     echo "\n Арихв создан\n";
-// } else {
-//     echo 'Архив не создан, код ошибки: ', $create;
-//     fwrite(STDERR, "Error while creating archive file");
-//     exit(1);
-// }
+    if ( $create === TRUE) {
+        echo "\n Арихв создан\n";
+    } else {
+        echo 'Архив не создан, код ошибки: ', $create;
+        fwrite(STDERR, "Error while creating archive file");
+        exit(1);
+    }
 
-// # loop through each file
-// foreach ($files as $file) {
-//     # download file
-//     if(file_exists($file)){
-//         #add it to the zip
-//         $zip->addFile($file, basename($file));
-//     }
-// }
+    # loop through each file
+    foreach ($files as $file) {
+        # download file
+        if(file_exists($file)){
+            #add it to the zip
+            $zip->addFile($file, basename($file));
+        }
+    }
 
-// # close zip
-// if($zip->close()){
-    
-// }
+    # close zip
+    if($zip->close()){
+        foreach ($files as $file) {
+            # download file
+            if(file_exists($file)){
+                #add it to the zip
+                unlink($file);
+            }
+        }
+    }
+}
 
