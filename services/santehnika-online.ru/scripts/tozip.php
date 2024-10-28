d6858041a585c7eb83d31f4bcd72009cd5b50e0f
Nikola <nikola.vls@uandex.ru>
2024-10-28 11:44:53 +0400

    fix

--- services/santehnika-online.ru/scripts/tozip.php
+++ services/santehnika-online.ru/scripts/tozip.php
@@ -5,7 +5,7 @@ use App\Models\Process\ProcessModel;
 require(dirname(__DIR__, 1)."/config/bootstrap.php");
 
 $page = 1;
-$limit = 4000;
+$limit = 100;
 $process = new ProcessModel();
 $processes = $process->query()->select()->from($process::getTableName())->where("status_id", 4)->paginationLimit($page, $limit)->fetchArrays();
 $files = [];
