301e9fac567141c5563e3db2101fe6b4fa124c85
Nikola <nikola.vls@uandex.ru>
2024-11-19 14:21:42 +0400

    fix

--- services/loader/app/index.ts
+++ services/loader/app/index.ts
@@ -29,7 +29,7 @@ logger.info("Программа запуущена")
 // worker.run()
 
 worksConfig.forEach((workerData) => {
-    const worker = new Worker('./worker.process.js', { workerData });
+    const worker = new Worker('dist/libworker.process.js', { workerData });
     worker.on('message', (message) => {
         logger.info(message)
     });
