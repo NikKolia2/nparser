af4961317b5da5f74b57fb849fb83c23a08d88cb
Nikola <nikola.vls@uandex.ru>
2024-11-25 12:42:31 +0400

    fix

--- services/parser/export_old.php
+++ services/parser/export_old.php
@@ -14,7 +14,7 @@ export($categories, $pathToSave);
 
 function export($categories, $pathToSave){
     foreach ($categories as $category) {
-        $pathToSave = $pathToSave.iconv("UTF-8", "CP1251", $category["h1"]);
+        $pathToSave = $pathToSave.iconv("UTF-8", "UCS-2", $category["h1"]);
         mkdir($pathToSave);
         return 0;
         exportByCateglry($category["id"], $pathToSave);
