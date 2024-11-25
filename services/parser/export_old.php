e68681daddd8ac17d63441ebb416f03d907cf45d
Nikola <nikola.vls@uandex.ru>
2024-11-25 13:29:23 +0400

    fix

--- services/parser/export_old.php
+++ services/parser/export_old.php
@@ -15,7 +15,7 @@ export($categories, $pathToSave);
 function export($categories, $pathToSave){
     foreach ($categories as $category) {
         
-        $pathToSave = $pathToSave.HelperService::translite($category["h1"]));
+        $pathToSave = $pathToSave.HelperService::translite($category["h1"]);
        
         exportByCateglry($category["id"], $pathToSave);
         $c = getChildrenCategories($category["id"]);
